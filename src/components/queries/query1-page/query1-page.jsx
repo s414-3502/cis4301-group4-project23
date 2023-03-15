009d3c4f7272fc78d03e4054fe1103b076c3b9a2
Maanasi Vadlamani <maanasivadlamani@gmail.com>
2023-03-15 18:50:17 -0400

    padding is good for scroll and interpretation and data visualization box setup for queries

--- src/components/queries/query1-page/query1-page.jsx
+++ src/components/queries/query1-page/query1-page.jsx
@@ -1,10 +1,21 @@
 import "./query1-page.css";
 import React from "react";
 
+import Box from '@mui/material/Box';
+
 function template() {
   return (
     <div className="query-1-page">
-      <h1>query1-page</h1>
+      <Box sx={{ flexGrow: 1,  height: 700}}>
+        <Box class="dataVisualization"> 
+        </Box>
+        <hr />
+        <Box class="interpretation"> 
+        </Box>
+        <Box>
+        </Box>
+      </Box>
+
     </div>
   );
 };
