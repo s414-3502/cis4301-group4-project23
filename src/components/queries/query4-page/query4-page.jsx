502856568f97516ac50b4841c1c1669e468eac1c
Maanasi Vadlamani <maanasivadlamani@gmail.com>
2023-04-10 21:45:34 -0400

    perfected static ui on query 4

--- src/components/queries/query4-page/query4-page.jsx
+++ src/components/queries/query4-page/query4-page.jsx
@@ -16,7 +16,7 @@ function Template() {
   };
   return (
     <div className="query-4-page">
-      <Box sx={{ flexGrow: 1,  height: 700}}>
+      <Box sx={{ flexGrow: 1,  height: 1000}}>
         <h1>The Connection Between Time, Premises, and Crime Type</h1>
         <Divider sx={{mb: 1.5, mt: 3, "&::before, &::after": {borderColor: "#7c76a3",}, }}>
           <Typography sx={{color:"#484273", fontSize: 13,}}>
@@ -30,17 +30,17 @@ function Template() {
         >
           <Box sx={{width: '80%', border: 1, borderColor: 'gray', borderRadius:3,}}>
           <Plot
-            data={[
-              {
-                x: [1, 2, 3],
-                y: [2, 6, 3],
-                type: 'scatter',
-                mode: 'lines+markers',
-                marker: {color: 'red'},
-              },
-              {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-            ]}
-            layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+              data={[
+                {
+                  x: ["2013-10-04 22:23:00", "2013-11-04 22:23:00", "2013-12-04 22:23:00"],
+                  y: [1, 3, 6],
+                  type: 'scatter',
+                  mode: 'lines+markers',
+                  marker: {color: 'purple'},
+                },
+              ]
+              }
+              layout={ {width: 1200, height: 600} }
           />
           </Box>
           <Box sx={{display: 'flex', flexDirection:'column', mr:-3, ml:1,}}>
@@ -122,24 +122,6 @@ function Template() {
               </FormControl>
             </Box>
           </Box>
-        </Box>
-        <hr class="section-divider"/>
-        <h2>DATA ANALYSIS</h2>
-        <Box sx={{backgroundColor: "#D5D1ED", mx:15, p:2, pt:0.1, borderRadius:3}}> 
-          <h3>Data Analysis and Interpretation</h3>
-          <p class="normal">Here is where a summary of the analysis of the data and the results will go. 
-            Of course, this section can only be completed after we have made the actual 
-            trend analysis for the web application. Only after deriving the results can 
-            we make an educated analysis of the data and the connections within it.
-          </p>
-          <p class="normal">
-          Here is where a summary of the analysis of the data and the results will go. 
-          Of course, this section can only be completed after we have made the actual trend 
-          analysis for the web application. Only after deriving the results can we make an 
-          educated analysis of the data and the connections within it.
-          </p>
-        </Box>
-        <Box>
         </Box>
       </Box>
     </div>
