2957de01657c71c08a642d3d9584e03a5b2c3019
ananya0809 <ananya.199303010@muj.manipal.edu>
2023-04-16 01:40:20 -0400

    cleanup internal project folder

--- src/components/queries/query1-page/query1-page.jsx
+++ src/components/queries/query1-page/query1-page.jsx
@@ -33,8 +33,8 @@ function Template() {
                 <Plot
                   data={[
                     {
-                      x: ["2013-10-04 22:23:00", "2013-11-04 22:23:00", "2013-12-04 22:23:00"],
-                      y: [1, 3, 6],
+                      x: ["2013-10-04 22:23:00", "2013-11-04 22:23:00", "2013-12-04 22:23:00"], //weeks
+                      y: [1, 3, 6], //percentage change
                       type: 'scatter',
                       mode: 'lines+markers',
                       marker: {color: 'purple'},
